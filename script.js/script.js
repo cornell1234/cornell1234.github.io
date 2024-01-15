9107b0585ad12fa39bb931420e9b4aaa7ff01b18
cornellchisao <cornellzchisao@gmail.com>
2024-01-15 21:38:31 +0200

    contact

--- script.js/script.js
+++ script.js/script.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function (){
     const icon = document.querySelector(".icon");
     const navList = document.querySelector("nav ul");
 
-    //Toggle the navigation menu wen the hamburger icon is clicked
+    //Toggle the navigation menu when the hamburger icon is clicked
     icon.addEventListener("click", function () {
         navList.classList.toggle("show");
     });
 });
+
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+}
